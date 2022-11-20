71c58b1e1ae05c38fc0daffc72cd45a616752337
Andrew Lyubin <alyubin@rtc.usetech.ru>
2022-11-20 23:09:06 +0400

    adding class

---
+++ src/test/java/git/prikol/class_andrew.java
@@ -0,0 +1,25 @@
+package git.prikol;
+
+public class class_andrew {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "class_andrew{" +
+                "id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
